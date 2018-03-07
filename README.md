7b9acc144bba6c45cfcf809749f1af8f9af918f9
JontyMorris <jonty.morris@yahoo.co.nz>
2018-03-07 20:55:01 +1300

    Displays example alerts, basic edit page

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ BrowserBoard is the homepage you never had. It will keep track of your favourite
 ## Getting setup
 You will need to have a version of Python 3 and Django installed in order to run BrowserBoard. It is also recommended that you make BrowserBoard your homepage in whatever browser you're using.
 
-### Installing Django
-You can install Django using Pip which comes with most Python installations.
+The simpliest way to install Django is via pip.
 ```
 pip install django
 ```
 
+To run the server, cd to the BrowserBoard directory and then run the following command.
+```
+python manage.py runserver
+```
--- home/views.py
+++ home/views.py
@@ -2,7 +2,55 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'home/index.html')
+	context = {"updates": [
+			{
+				"id": "1",
+				"url": "https://www.mangapanda.com/one-piece/896",
+				"img": "https://s3.mangapanda.com/cover/one-piece/one-piece-l1.jpg"
+			},
+			{
+				"id": "2",
+				"url": "https://www.mangapanda.com/black-clover/147",
+				"img": "https://s3.mangapanda.com/cover/black-clover/black-clover-l0.jpg"
+			},
+			{
+				"id": "3",
+				"url": "kissanime.ru/Anime/Dragon-Ball-Super/Episode-129?id=143543",
+				"img": "https://myanimelist.cdn-dena.com/images/anime/7/74606.jpg"
+			},
+			{
+				"id": "4",
+				"url": "http://kissanime.ru/Anime/Overlord-II/Episode-009?id=143595&s=beta&pfail=1",
+				"img": "http://kissanime.ru/Uploads/Etc/11-30-2017/19597854588022l.jpg"
+			}
+		]
+	}
+
+	return render(request, 'home/index.html', context)
 
 def edit(request):
-	return render(request, 'home/edit.html')
+	context = {"updates": [
+			{
+				"id": "1",
+				"url": "https://www.mangapanda.com/one-piece",
+				"img": "https://s3.mangapanda.com/cover/one-piece/one-piece-l1.jpg",
+			},
+			{
+				"id": "2",
+				"url": "https://www.mangapanda.com/black-clover",
+				"img": "https://s3.mangapanda.com/cover/black-clover/black-clover-l0.jpg"
+			},
+			{
+				"id": "3",
+				"url": "kissanime.ru/Anime/Dragon-Ball-Super",
+				"img": "https://myanimelist.cdn-dena.com/images/anime/7/74606.jpg"
+			},
+			{
+				"id": "4",
+				"url": "http://kissanime.ru/Anime/Overlord-II",
+				"img": "http://kissanime.ru/Uploads/Etc/11-30-2017/19597854588022l.jpg"
+			}
+		]
+	}
+
+	return render(request, 'home/edit.html', context)
